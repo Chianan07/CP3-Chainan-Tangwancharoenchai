939aa769aeec75cd6872d94fafb097bf5456b031
Chianan07 <82432879+Chianan07@users.noreply.github.com>
2021-04-13 16:57:54 +0700

    Create Exercise4_Chainan_T.py

---
+++ Exercise4_Chainan_T.py
@@ -0,0 +1,10 @@
+f = "Foundation English"
+g = "General Business"
+i = "Introduction to computer system"
+c = "Computer Programming"
+x = "Score"
+print("---Your Score---")
+print(f, ":", x)
+print(g, ":", x)
+print(i, ":", x)
+print(c, ":", x)
