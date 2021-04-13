3e79a5bf6ccb268691c345d3cc92a93afdc60848
Chianan07 <82432879+Chianan07@users.noreply.github.com>
2021-04-13 18:43:59 +0700

    Create Exercise5_Chainan_t.py

---
+++ Exercise5_Chainan_t.py
@@ -0,0 +1,10 @@
+x = int(input("First Number :"))
+y = int(input("Second Number :"))
+result1 = x+y
+result2 = x-y
+result3 = x*y
+result4 = int(x/y)
+print(x, "+", y, "=", result1)
+print(x, "-", y, "=", result2)
+print(x, "*", y, "=", result3)
+print(x, "/", y, "=", result4)  
