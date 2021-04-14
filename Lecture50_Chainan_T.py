c5989b62d3b85a33442479943fa9339c45c8a805
Chianan07 <82432879+Chianan07@users.noreply.github.com>
2021-04-14 22:35:07 +0700

    Create Lecture50_Chainan_T.py

---
+++ Lecture50_Chainan_T.py
@@ -0,0 +1,13 @@
+def plusNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def dividedNumber(x,y):
+    print(x/y)
+
+plusNumber(50, 25)
+minusNumber(20, 10)
+multiplyNumber(10, 5)
+dividedNumber(5, 1)
